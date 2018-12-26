bd5a18048e0cfa9584b2f5b9454eac75929675f2
Rene Ventura <venturar729@gmail.com>
2018-12-26 04:19:18 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,68 @@
 # your code goes here
+def begins_with_r(array)
+  array.each do |ele|
+    if ele[0] != "r"
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(array)
+  array.select { |ele| ele.include?("a") }
+end
+
+def first_wa(array)
+  array.find { |ele| ele[0..1] == "wa"}
+end
+
+def remove_non_strings(array)
+  array.delete_if {|ele| !ele.is_a?(String)}
+end
+
+def count_elements(array)
+  count = Hash.new {0}
+  count_hash = {}
+  count_array = []
+  array.each do |ele|
+    count[ele] += 1
+    count_hash[ele] = {:count => count[ele]}
+    if ele.class == Hash
+      ele.each { |k,v| count_hash[ele][k] = v}
+    else
+      count_hash[ele][:name] = ele
+    end
+  end
+  count_hash.values
+end
+
+def merge_data(keys, data)
+  merged_data = []
+  data.each do |ele|
+    keys.each do |key|
+      key.each do |k, v|
+        ele[v][k] = v
+        merged_data << ele[v]
+      end
+    end
+  end
+  merged_data
+end
+
+def find_cool(hash_array)
+  hash_array.select do |hash|
+    hash[:temperature] == "cool"
+  end
+end
+
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |k, v|
+    if organized_schools[v[:location]] == nil
+      organized_schools[v[:location]] = [k]
+    else
+      organized_schools[v[:location]] << k
+    end
+  end
+  organized_schools
+end
